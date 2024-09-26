44b2d140f9b227235a9d5b26de946f9bfdf03a05
GalDotCom <gal.liberal.garcia@gmail.com>
2024-09-26 17:07:35 -0600

    Update meme.py

--- meme.py
+++ meme.py
@@ -14,4 +14,16 @@ async def mem(ctx):
     with open(f'images/{img_name}', 'rb') as f:
         picture = discord.File(f)
     await ctx.send(file=picture)
+    
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command()
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
 bot.run('token')
